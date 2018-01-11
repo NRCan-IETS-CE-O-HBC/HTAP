f59403aea9167acc55d19c79e9e6b8bf8dd33173
fergusoa <alex.ferguson@canada.ca>
2018-01-11 14:23:57 -0500

    Deleting unnecessary genopt files

--- applications/NBC936/genopt-run-auto.bat
+++
@@ -1,8 +0,0 @@
-java -classpath genopt.jar genopt.GenOpt Genopt-run-auto-1++.GO-ini
-timeout /t 10 
-copy OutputListingAll.txt CloudResultsBatch1.txt
-del OutputListingAll.txt
-java -classpath genopt.jar genopt.GenOpt Genopt-run-auto-2++.GO-ini
-timeout /t 10 
-copy OutputListingAll.txt CloudResultsBatch2.txt
-del OutputListingAll.txt
