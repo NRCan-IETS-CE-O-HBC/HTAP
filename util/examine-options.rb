871539941071d2f2bff44d4ec3f994a757cd60e0
fergusoa <alex.ferguson@canada.ca>
2019-01-15 21:26:45 -0500

    Changes to code for working with HTAP-options.jsom

--- util/examine-options.rb
+++ util/examine-options.rb
@@ -17,12 +17,25 @@ require_relative '../include/costing'
 require_relative '../include/legacy-code'
 require_relative '../include/constants'
 
-hasCostData = Hash.new
+$program = "examine-options.rb"
+$info    = 
+"  This script opens up the htap-options.json file and
+  perfroms a set of tasks with it:
 
+  With these modifications, it creates a new version of
+  the options file (HTAP-options-draft.json), and
+  saves it to the working directory.
+
+
+"
+
+hasCostData = Hash.new
+setTermSize
+writeHeader
 
 optionsFile = File.read("C:\\HTAP\\HTAP-options.json")
 optionsHash = JSON.parse(optionsFile)
-
+exit
 optionsHash.each do | attribute, definitions |
   # Report cost data
 
@@ -30,19 +43,19 @@ optionsHash.each do | attribute, definitions |
   #
   # =======================================================================
 
-
+  stream_out drawRuler("attribute #{attribute}")
   if ( ! definitions["costed"] ) then
-    stream_out "> attribute #{attribute} : no cost data\n"
+    stream_out "(no cost data)\n"
     next
-
   end
 
   definitions["options"].each do | option, data |
 
+     stream_out drawRuler(" -> #{option}  ? ",". ")
      if ( ! data["costs"]["components"].nil? &&
             data["costs"]["components"].length > 0 ) then
 
-         stream_out " Cost data for :  #{attribute} \ #{option} \n"
+         stream_out "       found component cost data:"
          if ( hasCostData[attribute].nil? )
            hasCostData[attribute] = "#{option}"
          else
@@ -50,7 +63,18 @@ optionsHash.each do | attribute, definitions |
          end
 
      end
+    if ( ! data["costs"]["proxy"].nil? )
+      proxy = data["costs"]["proxy"]
+      stream_out "       found component proxy cost data: (#{proxy})"
+      if ( hasCostData[attribute].nil? )
+         hasCostData[attribute] = "#{option}"
+       else
+         hasCostData[attribute] = "#{hasCostData[attribute]}, #{option}"
+      end
+    end
 
+
+    stream_out "\n"
   end
 
 end
@@ -63,4 +87,4 @@ outputCDf = File.new("options_with_cost_data.txt", "w")
 hasCostData.each do |attribute,options|
   outputCDf.write "#{attribute} => #{options} \n"
 end
-outputCDf.close 
+outputCDf.close
